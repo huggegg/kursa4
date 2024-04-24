2abe4163e84301c8e0e304ebeed1800ea04b20be
Hugge <hugge_gaming@mail.ru>
2024-04-24 20:39:27 +0300

    full edition

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 document.getElementById("openModal").addEventListener("click", function(){
     document.getElementById("modal").classList.remove('collapse')
 })
+
+document.getElementById("closeModal").addEventListener("click",function(){
+    document.getElementById("modal").classList.add("collapse")
+})
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        document.getElementById("modal").classList.add("collapse")
+    }
+});
+
